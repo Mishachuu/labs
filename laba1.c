0f8bc642b33a59da771db46d9cdaae1e397a2024
Миша Чукарев <mishakrut11@gmail.com>
2024-03-23 09:48:37 +0400

    update

--- laba1.c
+++ laba1.c
@@ -8,27 +8,34 @@
 
 int main() {
     // Создание каталогов
-    mkdir("a", 0755);
+    mkdir("a_0", 0755);
+    mkdir("a_2", 0755);
+    mkdir("a_2/b_0", 0755);
+    mkdir("a_2/b_2", 0755);
+    mkdir("a_0/b_3", 0755);
+    mkdir("a_0/b_3/c_3", 0755);
 
-    char* catText = "catts";
-    int catFile = open("a/cat.txt", O_CREAT | O_WRONLY, 0644);
-    write(catFile, catText, strlen(catText));
-    close(catFile);
-
-    // Создание пустого файла dog.txt
-    int dogFile = open("a/c/dog.txt", O_CREAT, 0644);
+    symlink("a_0", "a_1");
+    
+    char* dogText = "dog";
+    int dogFile = open("a_0/b_1.txt", O_CREAT | O_WRONLY, 0644);
+    write(dogFile, dogText, strlen(dogText));
     close(dogFile);
 
 
     // Создание файла g.bin с 500 байтами случайных данных
-    int gFile = open("a/g.bin", O_CREAT | O_WRONLY, 0644);
-    char randomData[500];
+    int c0File = open("a_2/b_2/c_0.bin", O_CREAT | O_WRONLY, 0644);
+    char ZeroData[909];
     
-    int urandom_fd = open("/dev/urandom", O_RDONLY);
-    ssize_t filling = read(urandom_fd, randomData, sizeof(randomData));
-    write(gFile, randomData, sizeof(randomData));
-    close(gFile);
-    close(urandom_fd);
+    int zero_fd = open("/dev/zero", O_RDONLY);
+    ssize_t filling = read(zero_fd, ZeroData, sizeof(ZeroData));
+    write(c0File, ZeroData, sizeof(ZeroData));
+    close(c0File);
+    close(zero_fd);
+ 
+    symlink("a_2/b_2/c_0.bin", "a_2/b_2/c_1.bin");
+
+    link("a_2/b_2/c_0.bin", "a_0/b_3/c_2.bin");
 
     return 0;
 }
